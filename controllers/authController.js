00af651b3069eb0145a2dc5e05af3919748c3458
bharath <bharathhalemane143@gmail.com>
2026-01-27 23:17:23 +0530

    login api done

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,9 @@ const bcrypt = require("bcryptjs");
 
 exports.signup = async (req, res) => {
     try {
-        const { name, email, password, confirmPassword } = req.body;
+          console.log('REQ BODY 👉', req.body)
+        const { name, email, password, confirmPassword } = req.body || {};
+        
 
         if (!name || !email || !password || !confirmPassword) {
             return res.status(400).json({ message: "All fields are required" });
@@ -20,20 +22,45 @@ exports.signup = async (req, res) => {
         }
 
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const jwtToken = jwt.sign(
-            { id: email },
-            process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRE }
-        );
+        const hashedPassword = await bcrypt.hash(password, salt);        
 
         const newUser = await User.create({
-            name, email, password: hashedPassword, jwt_token: jwtToken
+            name, email, password: hashedPassword
         })
 
-        res.status(201).json({ message: "User registered successfully", userId: newUser._id, jwtToken });
+        res.status(201).json({ message: "User registered successfully", userId: newUser._id});
     } catch (err) {
+         console.error("SIGNUP ERROR 👉", err);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid Password" });
+        }
+
+        const payload = {
+            userId: user._id,
+        }
+
+        const jwt_token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+        res.status(200).json({ jwt_token, userId: user._id });
+    } catch (err) {
+        console.error("login ERROR 👉", err);
         res.status(500).json({ message: "Server error" });
     }
 }
--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
+const cors = require("cors")
 
 dotenv.config()
-
 connectDB()
 
-const app = express() 
+const app = express()
 
-app.use(express.json())
+app.use(cors())
+app.use(express.json())        
+app.use(express.urlencoded({ extended: true })) 
 
 app.use("/api/auth", require("./routes/authRoutes"))
 
-const PORT = process.env.PORT || 5000 
-
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
-
-app.get('/force', async (req, res) => {
-  const User = require('./models/User')
-
-  const user = await User.create({
-    name: 'FORCE USER',
-    email: `force${Date.now()}@gmail.com`,
-    password: 'forcepass'
-  })
-
-  res.json(user)
-})
